c61941862cfe7e9f9b456cff43091f30a0c62edc
Tim Seyschab <tim@shellnuts.de>
2021-09-17 23:17:30 +0200

    Async Rocket starter mit Elastic Anbindung

---
+++ apps/shellnuts-backend/src/elastic/elastic.rs
@@ -0,0 +1,22 @@
+use elasticsearch::{http::transport::Transport, Elasticsearch};
+use log::info;
+
+pub(crate) trait WithElastic {
+  fn elasticsearch(&self) -> &Elasticsearch;
+}
+
+///
+/// # Panics
+///
+/// Panics if it cannot reach elastic search
+pub(crate) async fn new() -> Elasticsearch {
+  let es_url = "http://localhost:9200/";
+  let es_transport = Transport::single_node(&es_url).unwrap();
+  let elasticsearch = Elasticsearch::new(es_transport);
+
+  let es_result = elasticsearch.ping().send().await;
+  let es_resp = es_result.map_err(|e| format!("Cannot ping Elasticsearch: {}.", e)).unwrap();
+  info!("Elasticsearch ping status: {}.", es_resp.status_code());
+
+  elasticsearch
+}
--- apps/shellnuts-backend/src/main.rs
+++ apps/shellnuts-backend/src/main.rs
@@ -1,11 +1,56 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use elasticsearch::Elasticsearch;
+use env_logger::DEFAULT_FILTER_ENV;
+use rocket::{State};
+use std::env;
+
+use crate::elastic::elastic::WithElastic;
+
+/// Module that needs initialisation on startup and a living state.
+mod elastic {
+  pub(crate) mod elastic;
+}
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+  "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main() {
+  if env::var(DEFAULT_FILTER_ENV).is_err() {
+    env::set_var(DEFAULT_FILTER_ENV, "info");
+  }
+  env_logger::init();
+
+  let app_state = App::new().await;
+
+  rocket::build()
+    .manage(app_state)
+    .mount("/", routes![index])
+    .launch()
+    .await
+    .expect("Rocket is launching")
+}
+
+struct App {
+  elasticsearch: Elasticsearch,
+}
+
+type AppState<'a> = State<App>;
+
+impl App {
+  async fn new() -> Self {
+    let elasticsearch = elastic::elastic::new().await;
+
+    Self { elasticsearch }
+  }
+}
+
+impl WithElastic for AppState<'_> {
+  fn elasticsearch(&self) -> &Elasticsearch {
+    &self.elasticsearch
+  }
 }
